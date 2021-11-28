c5ed26ac280cd903e62e0d5f387307132a9faee6
Wayne Munro <964342+waynemunro@users.noreply.github.com>
2021-11-28 14:05:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+
+Console.WriteLine("Press a key");
+
+Console.ReadKey();
+
+var code = File.ReadAllText("sayhello.txt");
+
+Console.WriteLine(code);
+
+
+
+Console.WriteLine("Press a key");
+
+while (true)
+{
+    Console.ReadKey();
+
+    var scriptCode1 = File.ReadAllText("scriptCode1.txt");
+
+    var scriptCode = CSharpScript.Create(scriptCode1);
+
+    scriptCode.Compile();
+
+    ;
+
+    await scriptCode.RunAsync();
+
+}
+
+
+
+
+
+
+
